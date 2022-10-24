3394e259497f52f7decc5301a80ce09ba48098c6
Mariana Ruiz <m.ruizg@uniandes.edu.co>
2022-10-23 20:33:47 -0500

    Todo funciona!

--- main.ts
+++ main.ts
@@ -1,24 +1,39 @@
 import { Serie } from "./serie.js";
 
-import { series } from "./dataseries.js";
+import { dataSeries } from "./dataseries.js";
 
-let coursesTbody: HTMLElement = document.getElementById("series")!;
+const coursesTbody: HTMLElement = document.getElementById("series")!;
 const AvgSeasonsElm: HTMLElement = document.getElementById("seasons-avg")!;
+let serieCard: HTMLElement = document.getElementById("card")!;
 
-renderSeriesInTable(series);
-AvgSeasonsElm.innerHTML = `Seasons average: ${getAvgSeasons(series)}`
+
+renderSeriesInTable(dataSeries);
+AvgSeasonsElm.innerHTML = `Seasons average: ${getAvgSeasons(dataSeries)}`
 
 function renderSeriesInTable(series: Serie[]): void {
     console.log('Desplegando series');
     series.forEach((serie) => {
       let trElement = document.createElement("tr");
       trElement.innerHTML = `<td class="table-active bolded">${serie.id}</td>
-                             <td class="table-active blueName ">${serie.name}</td>
+                             <td class="table-active"><a href="#" class="stretched-link" id="serie ${serie.id}">${serie.name}</a></td>
                              <td class="table-active">${serie.channel}</td>
                              <td class="table-active">${serie.seasons}</td>`;
       coursesTbody.appendChild(trElement);
     });
   }
+
+  const serie1: HTMLElement = document.getElementById("serie 1")!;
+  const serie2: HTMLElement = document.getElementById("serie 2")!;
+  const serie3: HTMLElement = document.getElementById("serie 3")!;
+  const serie4: HTMLElement = document.getElementById("serie 4")!;
+  const serie5: HTMLElement = document.getElementById("serie 5")!;
+  const serie6: HTMLElement = document.getElementById("serie 6")!;
+  serie1.onclick = () => showCard(1);
+  serie2.onclick = () => showCard(2);
+  serie3.onclick = () => showCard(3);
+  serie4.onclick = () => showCard(4);
+  serie5.onclick = () => showCard(5);
+  serie6.onclick = () => showCard(6);
 
   function getAvgSeasons(series: Serie[]): number {
     let totalSeasons: number = 0;
@@ -30,4 +45,33 @@ function renderSeriesInTable(series: Serie[]): void {
     });
     let avgSeasons: number = totalSeasons/totalSeries;
     return avgSeasons;
+  }
+
+  function clearCard() {
+    while (serieCard.hasChildNodes()) {
+      if (serieCard.firstChild != null) {
+        serieCard.removeChild(serieCard.firstChild);
+       
+      }
+    }
+  }
+
+  function searchSerieById(idKey: number, series: Serie[]) {
+    return idKey === 0 ? dataSeries : series.filter( s => 
+      s.id == idKey);
+  }
+
+  function showCard(id: number) { 
+    clearCard();
+    let series: Serie[] = searchSerieById(id, dataSeries);
+    series.forEach((serie) => {
+      let cardElement = document.createElement("card");
+      cardElement.innerHTML = `<img src= "./img/${serie.id}.jpg" class="card-img-top">
+                              <div class="card-body">
+                              <h5 class="card-title">${serie.name}</h5>
+                              <p class="card-text">${serie.description}</p>
+                              <a href=${serie.link}>${serie.link}</a>`;
+      serieCard.appendChild(cardElement);
+    });
+    
   }
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,29 @@
-import { series } from "./dataseries.js";
+import { dataSeries } from "./dataseries.js";
 var coursesTbody = document.getElementById("series");
 var AvgSeasonsElm = document.getElementById("seasons-avg");
-renderSeriesInTable(series);
-AvgSeasonsElm.innerHTML = "Seasons average: ".concat(getAvgSeasons(series));
+var serieCard = document.getElementById("card");
+renderSeriesInTable(dataSeries);
+AvgSeasonsElm.innerHTML = "Seasons average: ".concat(getAvgSeasons(dataSeries));
 function renderSeriesInTable(series) {
     console.log('Desplegando series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td class=\"table-active bolded\">".concat(serie.id, "</td>\n                             <td class=\"table-active blueName \">").concat(serie.name, "</td>\n                             <td class=\"table-active\">").concat(serie.channel, "</td>\n                             <td class=\"table-active\">").concat(serie.seasons, "</td>");
+        trElement.innerHTML = "<td class=\"table-active bolded\">".concat(serie.id, "</td>\n                             <td class=\"table-active\"><a href=\"#\" class=\"stretched-link\" id=\"serie ").concat(serie.id, "\">").concat(serie.name, "</a></td>\n                             <td class=\"table-active\">").concat(serie.channel, "</td>\n                             <td class=\"table-active\">").concat(serie.seasons, "</td>");
         coursesTbody.appendChild(trElement);
     });
 }
+var serie1 = document.getElementById("serie 1");
+var serie2 = document.getElementById("serie 2");
+var serie3 = document.getElementById("serie 3");
+var serie4 = document.getElementById("serie 4");
+var serie5 = document.getElementById("serie 5");
+var serie6 = document.getElementById("serie 6");
+serie1.onclick = function () { return showCard(1); };
+serie2.onclick = function () { return showCard(2); };
+serie3.onclick = function () { return showCard(3); };
+serie4.onclick = function () { return showCard(4); };
+serie5.onclick = function () { return showCard(5); };
+serie6.onclick = function () { return showCard(6); };
 function getAvgSeasons(series) {
     var totalSeasons = 0;
     var totalSeries = 0;
@@ -20,4 +33,25 @@ function getAvgSeasons(series) {
     });
     var avgSeasons = totalSeasons / totalSeries;
     return avgSeasons;
+}
+function clearCard() {
+    while (serieCard.hasChildNodes()) {
+        if (serieCard.firstChild != null) {
+            serieCard.removeChild(serieCard.firstChild);
+        }
+    }
+}
+function searchSerieById(idKey, series) {
+    return idKey === 0 ? dataSeries : series.filter(function (s) {
+        return s.id == idKey;
+    });
+}
+function showCard(id) {
+    clearCard();
+    var series = searchSerieById(id, dataSeries);
+    series.forEach(function (serie) {
+        var cardElement = document.createElement("card");
+        cardElement.innerHTML = "<img src= \"./img/".concat(serie.id, ".jpg\" class=\"card-img-top\">\n                              <div class=\"card-body\">\n                              <h5 class=\"card-title\">").concat(serie.name, "</h5>\n                              <p class=\"card-text\">").concat(serie.description, "</p>\n                              <a href=").concat(serie.link, ">").concat(serie.link, "</a>");
+        serieCard.appendChild(cardElement);
+    });
 }
